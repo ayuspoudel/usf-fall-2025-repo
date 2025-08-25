efe6ab0021ba395b1ce163fe64292e4395cc5bd4
Ayush Poudel <ayushpoudel@usf.edu>
2025-08-25 15:00:25 -0400

    Deploy Lambda and Image

--- infra-terraform/iam.tf
+++ infra-terraform/iam.tf
@@ -10,7 +10,7 @@ resource "aws_iam_role" "lambda_exec" {
     }]
   })
 }
-  
+   
 resource "aws_iam_role_policy_attachment" "basic" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
