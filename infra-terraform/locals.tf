674ef0a108ddbf23ac5a01d726afab13f4618d6e
Ayush Poudel <ayushpoudel@usf.edu>
2025-08-25 14:46:17 -0400

    Deploy Lambda and Image

--- infra-terraform/locals.tf
+++ infra-terraform/locals.tf
@@ -1,8 +1,12 @@
 locals {
     aws_region = "us-east-1"
+
+    state_bucket = "clodstco-terraform-state"
+    dynamodb_table = "terraform-locks"
+
     lambda_function_name = "usf-fall-2025-canvas-sync"
     lambda_handler = "index.handler"
     lambda_runtime = "nodejs18.x"
     lambda_timeout = 30
-
+   
 }
